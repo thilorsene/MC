7c96cf7da0614bcc3e3644226f71f81d5cac410e
Thilor sene <thilorsene@gmail.com>
2020-11-19 17:27:26 +0100

    first

--- MC/urls.py
+++ MC/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Montre_Connecte/', include('Montre_Connecte.urls')),
+    path('', include('Montre_Connecte.urls')),
 ]
