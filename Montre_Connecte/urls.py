061baad5437e4b090fb78ab40e8878358ace5aec
Thilor <47110444+thilorsene@users.noreply.github.com>
2021-01-12 13:25:35 +0100

    Update urls.py

--- Montre_Connecte/urls.py
+++ Montre_Connecte/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('participant', views.participant, name='participant'),
     path('detail<int:id>', views.detail, name='detail')
 ]
